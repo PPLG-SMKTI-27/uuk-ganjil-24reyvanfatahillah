31a63a0af89e1b011bf6fee822f730b7afa69305
earlyayunda21 <114724033+earlyayunda21@users.noreply.github.com>
2025-11-17 13:05:23 +0700

    Database Market_Parkir - Basis Data A

---
+++ market_perbelanjaan.sql
@@ -0,0 +1,98 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               8.0.30 - MySQL Community Server - GPL
+-- Server OS:                    Win64
+-- HeidiSQL Version:             12.1.0.6537
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+-- Dumping database structure for market
+CREATE DATABASE IF NOT EXISTS `market` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `market`;
+
+-- Dumping structure for table market.admin
+CREATE TABLE IF NOT EXISTS `admin` (
+  `id_admin` int NOT NULL AUTO_INCREMENT,
+  `id_user` int NOT NULL,
+  `nama_admin` varchar(100) NOT NULL,
+  PRIMARY KEY (`id_admin`),
+  KEY `id_user` (`id_user`),
+  CONSTRAINT `admin_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table market.kendaraan
+CREATE TABLE IF NOT EXISTS `kendaraan` (
+  `id_kendaraan` int NOT NULL AUTO_INCREMENT,
+  `roda_kendaraan` enum('Roda Dua','Roda Empat') NOT NULL,
+  `harga` decimal(11,2) NOT NULL,
+  PRIMARY KEY (`id_kendaraan`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table market.parkiran
+CREATE TABLE IF NOT EXISTS `parkiran` (
+  `id_parkir` int NOT NULL AUTO_INCREMENT,
+  `id_kendaraan` int NOT NULL,
+  `biaya` decimal(11,2) NOT NULL,
+  PRIMARY KEY (`id_parkir`),
+  KEY `id_kendaraan` (`id_kendaraan`),
+  CONSTRAINT `parkiran_ibfk_1` FOREIGN KEY (`id_kendaraan`) REFERENCES `kendaraan` (`id_kendaraan`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table market.petugas_loket
+CREATE TABLE IF NOT EXISTS `petugas_loket` (
+  `id_petugas` int NOT NULL AUTO_INCREMENT,
+  `id_user` int NOT NULL,
+  `nama_petugas` varchar(100) NOT NULL,
+  PRIMARY KEY (`id_petugas`),
+  KEY `id_user` (`id_user`),
+  CONSTRAINT `petugas_loket_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table market.transaksi_parkir
+CREATE TABLE IF NOT EXISTS `transaksi_parkir` (
+  `id_transaksi` int NOT NULL AUTO_INCREMENT,
+  `id_parkir` int NOT NULL,
+  `id_petugas` int NOT NULL,
+  `waktu_transaksi` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id_transaksi`),
+  KEY `id_parkir` (`id_parkir`),
+  KEY `id_petugas` (`id_petugas`),
+  CONSTRAINT `transaksi_parkir_ibfk_1` FOREIGN KEY (`id_parkir`) REFERENCES `parkiran` (`id_parkir`),
+  CONSTRAINT `transaksi_parkir_ibfk_2` FOREIGN KEY (`id_petugas`) REFERENCES `petugas_loket` (`id_petugas`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table market.users
+CREATE TABLE IF NOT EXISTS `users` (
+  `id_user` int NOT NULL AUTO_INCREMENT,
+  `nama` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` enum('admin','petugas loket') NOT NULL,
+  PRIMARY KEY (`id_user`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Data exporting was unselected.
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
